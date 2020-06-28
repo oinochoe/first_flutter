8a73fbf0800dc4560746d551da07bd4f79902fa0
yeongminkim <oinochoe@naver.com>
2020-06-28 21:01:03 +0900

    flutter gridView

--- lib/main.dart
+++ lib/main.dart
@@ -36,25 +36,27 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(_text + '`s world'),
       ),
-      body: ListView(scrollDirection: Axis.vertical, children: <Widget>[
-        ListTile(
-          leading: Icon(Icons.home),
-          title: Text('Home'),
-          trailing: Icon(Icons.navigate_next),
-          onTap: () {},
-        ),
-        ListTile(
-          leading: Icon(Icons.event),
-          title: Text('Event'),
-          trailing: Icon(Icons.navigate_next),
-          onTap: () {},
-        ),
-        ListTile(
-            leading: Icon(Icons.camera),
-            title: Text('Camera'),
-            trailing: Icon(Icons.navigate_next),
-            onTap: () {})
-      ]),
+      body: GridView.count(crossAxisCount: 2, // 열수
+          children: <Widget>[
+            Container(
+                color: Colors.red,
+                width: 100,
+                height: 100,
+                padding: const EdgeInsets.all(8.0),
+                margin: const EdgeInsets.all(8.0)),
+            Container(
+                color: Colors.green,
+                width: 100,
+                height: 100,
+                padding: const EdgeInsets.all(8.0),
+                margin: const EdgeInsets.all(8.0)),
+            Container(
+                color: Colors.blue,
+                width: 100,
+                height: 100,
+                padding: const EdgeInsets.all(8.0),
+                margin: const EdgeInsets.all(8.0))
+          ]),
       floatingActionButton: FloatingActionButton(
           onPressed: () {
             setState(() {
